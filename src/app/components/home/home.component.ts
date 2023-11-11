ed565438225d7f7ef421b42c87d1098d73ddc5e0
Matias Strick <strickmatias87@gmail.com>
2023-11-10 21:21:52 -0300

    estilo y background (landing)

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { NavBarComponent } from 'src/app/shared/nav-bar/nav-bar.component';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
