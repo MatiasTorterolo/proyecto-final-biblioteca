f331f69a56e3214fde3f520221f55aad8543ccda
Guillermo lorenzo <guillermolorenzo89@gmail.com>
2023-11-14 23:34:58 -0300

    terminado addBook, edditBook

--- src/app/components/admin/add-book/add-book.component.ts
+++ src/app/components/admin/add-book/add-book.component.ts
@@ -15,6 +15,37 @@ export class AddBookComponent {
   constructor(){}
 
   public createLibro(){
+    const url: string = '../assets/imagenes'
+
+    if (this.newLibro.imagenUrl != ''){
+      switch ( this.newLibro.imagenUrl ) {
+        case "Suspenso":
+          this.newLibro.imagenUrl = url + "/libroNuevo1.jpg";
+            break;
+        case "Novela":
+          this.newLibro.imagenUrl = url + "/libroNuevo2.jpg"
+            break;
+        case "Misterio":
+          this.newLibro.imagenUrl = url + "/libroNuevo3.jpg"
+            break;
+        case "Fantasía":
+          this.newLibro.imagenUrl = url + "/libroNuevo4.jpg"
+            break;
+        case "Ciencia ficción":
+          this.newLibro.imagenUrl = url + "/libroNuevo5.jpg"
+            break;
+        case "Historia":
+          this.newLibro.imagenUrl = url + "/libroNuevo6.jpg"
+            break;
+        case "Chinchulines":
+          this.newLibro.imagenUrl = url + "/libroNuevo7.jpg"
+            break;
+        default:
+          this.newLibro.imagenUrl = url + "/libritox.jpg"
+            break;
+      }
+    }
+    this.newLibro.disponibilidad = 1;
     this.libroToCreate.emit(this.newLibro);
   }
 }
--- src/app/components/admin/edit-book/edit-book.component.ts
+++ src/app/components/admin/edit-book/edit-book.component.ts
@@ -10,6 +10,7 @@ import { APIService } from 'src/app/core/services/api.service';
 })
 export class EditBookComponent {
   public libro: Libro = new Libro();
+  public nuevaImagen: string = '';
  
   constructor(@Inject(MAT_DIALOG_DATA) public data: any, private apiService: APIService, private dialogRef: MatDialogRef<EditBookComponent>) {
 
@@ -19,6 +20,11 @@ export class EditBookComponent {
   }
 
   public editLibro(){
+    const img: string = '../assets/imagenes/libritox.jpg'
+    if (this.nuevaImagen != ''){
+      this.libro.imagenUrl = img;
+      console.log("imagen cambiada")
+    }
 
     this.apiService.editLibro(this.libro.id!, this.libro).subscribe({
       next: () => this.dialogRef.close(true),
