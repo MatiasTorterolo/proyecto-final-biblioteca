e75f4c4a05cffbfc670e8b7b08ba52654c45b31e
Guillermo lorenzo <guillermolorenzo89@gmail.com>
2023-11-17 16:59:33 -0300

    guardo footer header nav

--- src/app/shared/nav-bar/nav-bar.component.ts
+++ src/app/shared/nav-bar/nav-bar.component.ts
@@ -1,14 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit{
   nombre:string[];
 
   constructor() {
     this.nombre=["Juan"];
+  }
+
+  private nav: HTMLElement | null = null;
+
+  ngOnInit(): void {
+    this.nav = document.getElementById("contenedor-nav");
+
+    if (this.nav) {
+      const navOffsetTop: number = this.nav.offsetTop;
+
+      const fijarBarraNavegacion = (): void => {
+        if (window.pageYOffset > navOffsetTop) {
+          if (this.nav && this.nav.style) {
+            this.nav.style.position = "fixed";
+            this.nav.style.top = "0";
+          }
+        } else {
+          if (this.nav && this.nav.style) {
+            this.nav.style.position = "static";
+          }
+        }
+      };
+
+      window.addEventListener("scroll", fijarBarraNavegacion);
+    }
   }
 }
